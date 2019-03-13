c9088d60805379c1170e2d9e5acecacc47669607
Massimo Penzo <penzo.massimo@gmail.com>
2019-03-13 17:47:33 -0500

    Changed approach in requirements.txt

--- requirements.R
+++ requirements.R
@@ -1,12 +1,18 @@
+
 ## This is our requirements.R script
-## TODO : include an `InstalledPackage` fallback
-require(googleAnalyticsR)
-require(lubridate)
-require(dataframes2xls)
-require(ggthemr)
-require(plyr)
-require(dplyr) # need to load plyr before dplyr & not the other way!
-require(tidyr)
-require(stringr)
-# require(bigQueryR)
-require(janitor)
+
+project_packages <- c(
+	"lubridate",
+	"plyr",
+	"dplyr",
+	"tidyr"
+	)
+
+for (package in project_packages){
+	if (require(package) == FALSE) {
+		print("This package has already been installed xxxx")
+		install.packages(package)	
+	} else {
+		print("This package has already been installed")
+	} 
+}
